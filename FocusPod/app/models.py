54f8c19f7212570b8951772d998753434c986b95
Carlos Arro <carlosarro182@gmail.com>
2023-06-09 21:24:55 +0400

    Added styling and the task model

--- FocusPod/app/models.py
+++ FocusPod/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Task(models.Model):
+    task_content = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.task_content
+    
