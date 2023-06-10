db52997552cee0383140e1eefcf91dadcec0eca9
Carlos Arro <carlosarro182@gmail.com>
2023-06-10 17:07:50 +0400

    Added "app" in settings.py forms and views.py and updated the form features

--- FocusPod/app/views.py
+++ FocusPod/app/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
+
 # Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TaskForm()
+
     tasks = Task.objects.all()
-    context = {"tasks":tasks}
+    context = {"tasks":tasks,'form':form}
     return render(request,'app/app.html',context)
